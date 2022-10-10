c24c12d638c5ee70763ce4b7cd6d89f6b3f7076e
apurba2002 <debnathapurba582@gmail.com>
2022-10-10 17:12:15 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Prepare-a-C--program-for-adding-two-integer-numbers-using-Windows-from.j0b_no_02
+ 
---
+++ add two number/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace add_two_number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox2.Text);
+            double result = num1 + num2;
+            string show = "Result is :"+result;
+            label3.Text=show;
+            
+        }
+
+       
+    }
+}
